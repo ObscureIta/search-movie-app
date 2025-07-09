3628d57d34da276d00e7b4091dfbe1458c9e06ac
Ivan Fedorov <obscure.ita@gmail.com>
2025-07-09 11:17:30 +0300

    create base project struct

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.scss'
+
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <a href="/homepage" className="header__logo">
+          Кинолеееента
+        </a>
+      </header>
+    </>
+  )
+}
+
+export default Header
